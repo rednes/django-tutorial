5602e13a942a05ce12ff2bddbc7e9db8c206b027
rednes <528249+rednes@users.noreply.github.com>
2018-09-15 12:59:28 +0900

    Django 2.0 Girls tutorial

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,9 @@
 from django.db import models
-
-# Create your models here.
-
-from django.db import models
 from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
